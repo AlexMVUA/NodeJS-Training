944a1f719f5fa4cfda487f7fa260f33d3a5068ff
Alex Mykhailov <alexei.mykhaylov@gmail.com>
2020-02-21 14:31:30 +0200

    Refactoring, used pipeline for processing files.

--- tasks/app1-2.js
+++ tasks/app1-2.js
@@ -1,21 +1,25 @@
 const csv=require("csvtojson/v2");
-const csvFilePath='./csv/nodejs-hw1-ex1.csv'
+const csvFilePath='./csv/nodejs-hw1-ex1.csv';
+const outputTxtFile='./output/new_file2.txt';
 const fs = require('fs');
-var endOfLine = require('os').EOL;
+const { pipeline } = require('stream');
 
+pipeline(
+  csv({
+    colParser:{
+      "amount": "omit",
+  		"price":"number",
+  	},
+    headers: ['book','author','amount','price']
 
-csv()
-  .fromFile(csvFilePath)
-  .then((jsonObj)=>{
-	   console.log(jsonObj);
-     var contentToWrite = '';
-     for (var i = 0; i < jsonObj.length; i++) {
-       delete jsonObj[i].Amount;
-       let reducedObject = JSON.stringify(jsonObj[i]);
-       contentToWrite = contentToWrite + reducedObject + endOfLine;
-     }
-     fs.writeFile("./output/new_file2.txt", contentToWrite, (err) => {
-       if (err) throw err;
+  }).fromStream(fs.createReadStream(csvFilePath)),
+
+  fs.createWriteStream(outputTxtFile),
+  (err) => {
+    if (err) {
+      console.error('Pipeline failed.', err);
+    } else {
        console.log('file was successfully written');
-     });;
-  });
+    }
+  }
+);
