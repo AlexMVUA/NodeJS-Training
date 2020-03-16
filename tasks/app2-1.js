ea0e59800ab458b31e4fce6c637c61317dd1f558
Alex Mykhailov <alexei.mykhaylov@gmail.com>
2020-03-16 02:39:11 +0200

    Module 4. Task 4-2. Enabled CORS

--- tasks/app2-1.js
+++ tasks/app2-1.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import * as userRouter from './routes/users.js';
 import { logger, errorLogger } from './utils/logger.js';
+import cors from 'cors';
 
 const app = express();
 app.listen(3000);
 app.use(express.json());
+
+const corsOptions = { origin: false };
+app.use(cors(corsOptions));
 
 app.use(logger);
 app.use('/users/', userRouter.router);
