bd9b5cccd8c43964aaa265dfd3a1888c373da73b
Alex Mykhailov <alexei.mykhaylov@gmail.com>
2020-03-30 03:16:14 +0300

    Module 5. Task 5.4 Dotenv module for managing environment configuration

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,18 +1,17 @@
 import jwt from 'jsonwebtoken';
 import HttpStatus from 'http-status-codes';
-import { Constants } from './constants.js';
 
 export const Authentication = {
     signToken(response, user) {
         const payload = { 'user': user.login, 'isActive': user.isActive };
-        const token = jwt.sign(payload, 'secret_', { expiresIn: Constants.Configuration.EXPIRATION_TIME_SECONDS });
+        const token = jwt.sign(payload, process.env.ENV_SECRET, { expiresIn: process.env.ENV_EXPIRATION_TIME_SECONDS });
         response.send(token);
     },
     verifyToken(request, response, next) {
         const token = request.headers['x-access-token'];
 
         if (token) {
-            jwt.verify(token, 'secret_', (err) => {
+            jwt.verify(token, process.env.ENV_SECRET, (err) => {
                 if (err) {
                     response.status(HttpStatus.FORBIDDEN).send();
                 } else {
